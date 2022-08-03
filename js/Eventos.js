b14a6ca9c8c7441c9c94cf0420aee7ec998fbc51
Nicolas Soria <sorianicolas3786@gmail.com>
2022-08-03 18:50:32 -0300

    eventos

--- js/Eventos.js
+++ js/Eventos.js
@@ -1,4 +1,4 @@
-let eleccion = 2, eleccionTarj = 2, con = 0;
+let eleccion = 2, eleccionTarj = 2, con = 0, bandera;
 const tarjeta = document.getElementById ('card')
 const tarjetaCreditoHTML = document.getElementById ('card-credito')
 const lista = [];
@@ -9,9 +9,35 @@ function datosTarjeta (nombre, fechaNac, dni, banco, domicilio) {
     this.banco = prompt ('ingrese el nombre de su banco');
     this.domicilio = prompt ('ingrese su domicilio');
 }
+function denuncia () {
+    eleccion = prompt ('Ingrese 1 para confirmar denuncia, 0 para desestimar')
+    if (eleccion != 1 && eleccion != 0) {
+        youShallNotPass ()
+    }
+    if (eleccion == 1) {
+        bandera = true
+    } else {
+        bandera = false
+    }
+}
+function youShallNotPass () {
+    eleccion = prompt ('Opción equivocada : ingrese 1 para continuar, 0 para proceder a la siguiente instancia')
+    while (eleccion != 0 && eleccion != 1) {
+        eleccion = prompt ('Opción equivocada, vuelva a ingresar: 1 para continuar, 0 para proceder a la siguiente instancia que corresponda')
+    }
+    if (eleccion == 1) {
+        bandera = true
+    } 
+    if (eleccion == 0) {
+        bandera = false
+    }
+    if (eleccionTarj > 1 || eleccionTarj < 0) {
+        eleccionTarj = eleccion
+    }
+}
 eleccionTarj = prompt ('Bienvenido al simulador de Denuncia de Tarjetas ¿Desea denunciar su tarjeta de debito? Ingrese 1 para denunciar, Ingrese 0 para continuar con su tarjeta de credito')
-while (eleccionTarj != 0 && eleccionTarj != 1) {
-    eleccionTarj = prompt ('Ingreso una opción incorrecta. Ingrese 1 para denunciar su tarjeta de debito, Ingrese 0 para continuar con su tarjeta de credito')
+if (eleccionTarj != 1 && eleccionTarj != 0) {
+    youShallNotPass ()
 }
 if (eleccionTarj == 1) {
     const tarjetaDebito = new datosTarjeta ();
@@ -35,22 +61,25 @@ if (eleccionTarj == 1) {
         tarjeta.innerHTML = `<h2 class= "card__titulo">TARJETA DEBITO</h2> <span class="card__elementos">Titular: ${tarjetaDebito.nombre}</span>  <span  class="card__elementos">Banco: ${tarjetaDebito.banco}</span> <span  class="card__elementos">Fecha de nacimiento: ${tarjetaDebito.fechaNac}</span> <span  class="card__elementos">DNI: ${tarjetaDebito.dni}</span> <span  class="card__elementOS">Domicilio: ${tarjetaDebito.domicilio}</span>`
     }
     alert ('usted ha ingresado los siguientes datos: ' + lista.join ("\n"))
-    eleccion = prompt('Confirma la denuncia de la tarjeta de debito perteneciente al banco ' + lista[2] + ' a nombre de ' + lista[0] + '? Ingrese 1 para confirmar y 0 para desestimar')
-    while (eleccion != 0 && eleccion != 1) {
-        eleccion = prompt ('Opción equivocada vuelva a ingresar: 1 para confirmar denuncia, 0 para desestimarla.')
-    }
-    if (eleccion == 1) {
-        alert ('Su tarjeta ha sido denunciada, pongase en contacto con su banco para recibir información respectiva a la nueva')
+    denuncia ()
+    if (bandera == true) {
+        alert ('Su tarjeta ha sido denunciada, pongase en contacto con su banco para recibir información respectiva a la nueva, proseguimos con su tarjeta de credito')
         tarjeta.className = 'card-registrada-denunciada'
     }
-    if (eleccion == 0) {
-        alert ('Denuncia desestimada')
+    if (bandera == false) {
+        alert ('Denuncia desestimada, proseguimos con su tarjeta de credito')
     }
-    eleccion = prompt ('Proseguimos con su tarjeta de credito. Ingrese 1 si desea denunciar la misma; ingrese 0 para finalizar simulación')
-    while (eleccion != 0 && eleccion != 1) {
-        eleccion = prompt ('Ingreso una opción incorrecta. Ingrese 1 para denunciar su tarjeta de credito, Ingrese 0 para finalizar la simulación')
+    eleccion = prompt ('¿Desea denunciar su Tarjeta de credito? Ingrese 1 para continuar 0 para finalizar simulación')
+    if (eleccion != 1 && eleccion != 0) {
+        youShallNotPass ()
     }
     if (eleccion == 1) {
+        bandera = true
+    } else {
+        bandera = false
+    }
+    if (bandera == true) {
+        alert ('Registremos su Tarjeta de Credito')
         const tarjetaCredito = new datosTarjeta ();
         for (i = 0; i < 5; i++) {
             if (i == 0) {
@@ -72,21 +101,32 @@ if (eleccionTarj == 1) {
             tarjetaCreditoHTML.innerHTML = `<h2 class= "card__titulo">TARJETA CREDITO</h2> <span class="card__elementos">Titular: ${tarjetaCredito.nombre}</span>  <span  class="card__elementos">Banco: ${tarjetaCredito.banco}</span> <span  class="card__elementos">Fecha de nacimiento: ${tarjetaCredito.fechaNac}</span> <span  class="card__elementos">DNI: ${tarjetaCredito.dni}</span> <span  class="card__elementOS">Domicilio: ${tarjetaCredito.domicilio}</span>`
         }
         eleccion = prompt('Confirma la denuncia de la tarjeta de credito perteneciente al banco ' + lista[2] + ' a nombre de ' + lista[0] + '? Ingrese 1 para confirmar y 0 para desestimar')
-        while (eleccion != 0 && eleccion != 1) {
-            eleccion = prompt ('Opción equivocada vuelva a ingresar: 1 para confirmar denuncia, 0 para desestimarla.')
+        if (eleccion != 1 && eleccion != 0) {
+            youShallNotPass ()
         }
         if (eleccion == 1) {
+            bandera = true
+        } 
+        if (eleccion == 0) {
+            bandera = false
+        }
+        if (bandera == true) {
             alert ('Su tarjeta ha sido denunciada, pongase en contacto con su banco para recibir información respectiva a la nueva')
             tarjetaCreditoHTML.className = 'card-registrada-denunciada'
         }
     }
 }
 if (eleccionTarj == 0) {
-eleccion = prompt ('Proseguimos con su tarjeta de credito. Ingrese 1 si desea denunciar la misma; ingrese 0 para finalizar simulación')
-while (eleccion != 0 && eleccion != 1) {
-    eleccion = prompt ('Ingreso una opción incorrecta. Ingrese 1 para denunciar su tarjeta de credito, Ingrese 0 para finalizar la simulación')
-}
-    if (eleccion == 1) {
+    eleccion = prompt ('Proseguimos con su tarjeta de credito. Ingrese 1 si desea denunciar la misma; ingrese 0 para finalizar simulación')
+    if (eleccion != 1 && eleccion != 0) {
+        youShallNotPass ()
+    }
+    if (eleccion == 1 ) {
+        bandera = true
+    } else {
+        bandera = false
+    }
+    if (bandera == true) {
         const tarjetaCredito = new datosTarjeta ();
         for (i = 0; i < 5; i++) {
             if (i == 0) {
@@ -107,9 +147,9 @@ while (eleccion != 0 && eleccion != 1) {
             tarjetaCreditoHTML.className = 'card-registrada'
             tarjetaCreditoHTML.innerHTML = `<h2 class= "card__titulo">TARJETA CREDITO</h2> <span class="card__elementos">Titular: ${tarjetaCredito.nombre}</span>  <span  class="card__elementos">Banco: ${tarjetaCredito.banco}</span> <span  class="card__elementos">Fecha de nacimiento: ${tarjetaCredito.fechaNac}</span> <span  class="card__elementos">DNI: ${tarjetaCredito.dni}</span> <span  class="card__elementOS">Domicilio: ${tarjetaCredito.domicilio}</span>`
         }
-        eleccion = prompt('Confirma la denuncia de la tarjeta de debito perteneciente al banco ' + tarjetaCredito.banco + ' a nombre de ' + tarjetaCredito.nombre + '? Ingrese 1 para confirmar y 0 para desestimar')
-        while (eleccion != 0 && eleccion != 1) {
-            eleccion = prompt ('Opción equivocada vuelva a ingresar: 1 para confirmar denuncia, 0 para desestimarla.')
+        eleccion = prompt('Confirma la denuncia de la tarjeta de credito perteneciente al banco ' + tarjetaCredito.banco + ' a nombre de ' + tarjetaCredito.nombre + '? Ingrese 1 para confirmar y 0 para desestimar')
+        if (eleccion != 1 && eleccion != 0) {
+            youShallNotPass ()
         }
         if (eleccion == 1) {
             alert ('Su tarjeta ha sido denunciada, pongase en contacto con su banco para recibir información respectiva a la nueva')
