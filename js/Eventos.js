565c5779e047b2d92e2119bb5a02cd097b401cdb
Nicolas Soria <sorianicolas3786@gmail.com>
2022-08-10 16:56:07 -0300

    eventos-corregido

--- js/Eventos.js
+++ js/Eventos.js
@@ -1,6 +1,8 @@
 let eleccion = 2, eleccionTarj = 2, con = 0, bandera;
 const tarjeta = document.getElementById ('card')
 const tarjetaCreditoHTML = document.getElementById ('card-credito')
+const denunciaDebito = document.getElementById ('denunciaDebito')
+const denunciaCredito = document.getElementById ('denunciaCredito')
 const lista = [];
 function datosTarjeta (nombre, fechaNac, dni, banco, domicilio) {
     this.nombre = prompt ('ingrese su nombre');
@@ -19,6 +21,14 @@ function denuncia () {
     } else {
         bandera = false
     }
+}
+function denunciaDebitoEvento () {
+    alert ('Su tarjeta ha sido denunciada, pongase en contacto con su banco para recibir información respectiva a la nueva, proseguimos con su tarjeta de credito')
+    tarjeta.className = 'card-registrada-denunciada'
+}
+function denunciaCreditoEvento () {
+    alert ('Su tarjeta ha sido denunciada, pongase en contacto con su banco para recibir información respectiva a la nueva, proseguimos con su tarjeta de credito')
+    tarjetaCreditoHTML.className = 'card-registrada-denunciada'
 }
 function youShallNotPass () {
     eleccion = prompt ('Opción equivocada : ingrese 1 para continuar, 0 para proceder a la siguiente instancia')
@@ -40,6 +50,7 @@ if (eleccionTarj != 1 && eleccionTarj != 0) {
     youShallNotPass ()
 }
 if (eleccionTarj == 1) {
+    alert ('Registremos su tarjeta debito')
     const tarjetaDebito = new datosTarjeta ();
     for (i = 0; i < 5; i++) {
         if (i == 0) {
@@ -58,17 +69,11 @@ if (eleccionTarj == 1) {
             lista [i] = tarjetaDebito.domicilio
         }
         tarjeta.className = 'card-registrada'
+        denunciaDebito.className = 'denunciaDebito__habilitado'
         tarjeta.innerHTML = `<h2 class= "card__titulo">TARJETA DEBITO</h2> <span class="card__elementos">Titular: ${tarjetaDebito.nombre}</span>  <span  class="card__elementos">Banco: ${tarjetaDebito.banco}</span> <span  class="card__elementos">Fecha de nacimiento: ${tarjetaDebito.fechaNac}</span> <span  class="card__elementos">DNI: ${tarjetaDebito.dni}</span> <span  class="card__elementOS">Domicilio: ${tarjetaDebito.domicilio}</span>`
     }
     alert ('usted ha ingresado los siguientes datos: ' + lista.join ("\n"))
-    denuncia ()
-    if (bandera == true) {
-        alert ('Su tarjeta ha sido denunciada, pongase en contacto con su banco para recibir información respectiva a la nueva, proseguimos con su tarjeta de credito')
-        tarjeta.className = 'card-registrada-denunciada'
-    }
-    if (bandera == false) {
-        alert ('Denuncia desestimada, proseguimos con su tarjeta de credito')
-    }
+    denunciaDebito.addEventListener('click', denunciaDebitoEvento)
     eleccion = prompt ('¿Desea denunciar su Tarjeta de credito? Ingrese 1 para continuar 0 para finalizar simulación')
     if (eleccion != 1 && eleccion != 0) {
         youShallNotPass ()
@@ -97,23 +102,12 @@ if (eleccionTarj == 1) {
             if (i == 4) {
                 lista [i] = tarjetaCredito.domicilio
             }
+            denunciaCredito.className = 'denunciaCredito__habilitado'
             tarjetaCreditoHTML.className = 'card-registrada'
             tarjetaCreditoHTML.innerHTML = `<h2 class= "card__titulo">TARJETA CREDITO</h2> <span class="card__elementos">Titular: ${tarjetaCredito.nombre}</span>  <span  class="card__elementos">Banco: ${tarjetaCredito.banco}</span> <span  class="card__elementos">Fecha de nacimiento: ${tarjetaCredito.fechaNac}</span> <span  class="card__elementos">DNI: ${tarjetaCredito.dni}</span> <span  class="card__elementOS">Domicilio: ${tarjetaCredito.domicilio}</span>`
         }
-        eleccion = prompt('Confirma la denuncia de la tarjeta de credito perteneciente al banco ' + lista[2] + ' a nombre de ' + lista[0] + '? Ingrese 1 para confirmar y 0 para desestimar')
-        if (eleccion != 1 && eleccion != 0) {
-            youShallNotPass ()
-        }
-        if (eleccion == 1) {
-            bandera = true
-        } 
-        if (eleccion == 0) {
-            bandera = false
-        }
-        if (bandera == true) {
-            alert ('Su tarjeta ha sido denunciada, pongase en contacto con su banco para recibir información respectiva a la nueva')
-            tarjetaCreditoHTML.className = 'card-registrada-denunciada'
-        }
+        alert ('usted ha ingresado los siguientes datos: ' + lista.join ("\n"))
+        denunciaCredito.addEventListener('click', denunciaCreditoEvento)
     }
 }
 if (eleccionTarj == 0) {
@@ -144,19 +138,11 @@ if (eleccionTarj == 0) {
             if (i == 4) {
                 lista [i] = tarjetaCredito.domicilio
             }
+            denunciaCredito.className = 'denunciaCredito__habilitado'
             tarjetaCreditoHTML.className = 'card-registrada'
             tarjetaCreditoHTML.innerHTML = `<h2 class= "card__titulo">TARJETA CREDITO</h2> <span class="card__elementos">Titular: ${tarjetaCredito.nombre}</span>  <span  class="card__elementos">Banco: ${tarjetaCredito.banco}</span> <span  class="card__elementos">Fecha de nacimiento: ${tarjetaCredito.fechaNac}</span> <span  class="card__elementos">DNI: ${tarjetaCredito.dni}</span> <span  class="card__elementOS">Domicilio: ${tarjetaCredito.domicilio}</span>`
         }
-        eleccion = prompt('Confirma la denuncia de la tarjeta de credito perteneciente al banco ' + tarjetaCredito.banco + ' a nombre de ' + tarjetaCredito.nombre + '? Ingrese 1 para confirmar y 0 para desestimar')
-        if (eleccion != 1 && eleccion != 0) {
-            youShallNotPass ()
-        }
-        if (eleccion == 1) {
-            alert ('Su tarjeta ha sido denunciada, pongase en contacto con su banco para recibir información respectiva a la nueva')
-            tarjetaCreditoHTML.className = 'card-registrada-denunciada'
-        }
+        alert ('usted ha ingresado los siguientes datos: ' + lista.join ("\n"))
+        denunciaCredito.addEventListener('click', denunciaCreditoEvento)
     }
-}
-alert ('Fin de la simulación')
-
-
+} 
